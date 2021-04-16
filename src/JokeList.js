33e1c59d48eba8d4aa9636085ee49e420f731204
Jessica <jessica@Jessicas-MacBook-Pro.local>
2021-04-16 15:29:10 -0700

    fix get new joke button

--- src/JokeList.js
+++ src/JokeList.js
@@ -17,40 +17,44 @@ function JokeList({numJokesToGet}) {
 
   useEffect(() => {
     /* retrieve jokes from API */
-    async function getJokes() {
-      try {
-        // load jokes one at a time, adding not-yet-seen jokes
-        let j = [...jokes]
-        let seenJokes = new Set();
-  
-        while (j.length < numJokesToGet) {
-          let res = await axios.get("https://icanhazdadjoke.com", {
-            headers: { Accept: "application/json" }
-          });
-          let { ...joke } = res.data;
-  
-          if (!seenJokes.has(joke.id)) {
-            seenJokes.add(joke.id);
-            j.push({ ...joke, votes: 0 });
-          } else {
-            console.log("duplicate found!");
-          }
-        }
-        setJokes(j)
-        setIsLoading(false);
-      } catch (err) {
-        console.error(err);
-      }
-    }
+
     if (!jokes.length) getJokes()
   }, []);
 
+
+  async function getJokes() {
+    try {
+      // load jokes one at a time, adding not-yet-seen jokes
+      let j = []
+      let seenJokes = new Set();
+
+      while (j.length < numJokesToGet) {
+        let res = await axios.get("https://icanhazdadjoke.com", {
+          headers: { Accept: "application/json" }
+        });
+        let { ...joke } = res.data;
+
+        if (!seenJokes.has(joke.id)) {
+          seenJokes.add(joke.id);
+          j.push({ ...joke, votes: 0 });
+        } else {
+          console.log("duplicate found!");
+        }
+        console.log("J!", j)
+      }
+      setJokes(j)
+      setIsLoading(false);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   /* empty joke list, set to loading state, and then call getJokes */
 
   function generateNewJokes() {
     setJokes([])
     setIsLoading(true);
+    getJokes();
   }
 
   /* change vote for this id by delta (+1 or -1) */
